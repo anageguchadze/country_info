8ed9b16d15a507208cddeb7009482fd79447ca4e
Ana <anageguchadze22@gmail.com>
2025-05-06 15:47:47 +0400

    Add logging

--- country_app/views.py
+++ country_app/views.py
@@ -4,6 +4,11 @@ from .serializers import CountrySerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from .filters import CountryFilter
+from django.core.cache import cache
+import logging
+from rest_framework.response import Response
+
+logger = logging.getLogger('country_app')
 
 class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
@@ -11,6 +16,19 @@ class CountryList(generics.ListCreateAPIView):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filterset_class = CountryFilter
     search_fields = ['name', 'id']
+    def list(self, request, *args, **kwargs):
+        query_params = request.query_params
+        cache_key = f'country_list:{query_params}'
+        data = cache.get(cache_key)
+        if data:
+            logger.info('data retrieved from cache')
+        else:
+            logger.info('data retrieved from db')
+            filtered_queryset = self.filter_queryset(self.get_queryset())
+            data = CountrySerializer(filtered_queryset, many=True).data
+            cache_time = 60 * 5
+            cache.set(cache_key, data, cache_time)
+        return Response(data)
 
 
 class CountryDetail(generics.RetrieveUpdateDestroyAPIView):
--- country_pro/settings.py
+++ country_pro/settings.py
@@ -123,3 +123,40 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
+INTERNAL_APS = ['127.0.0.1',]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "country_app": {  # Your app's logger
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
